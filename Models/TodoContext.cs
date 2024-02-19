eb1830cd4a9334ea4b835e31459d8a5284eefacb
Zachary Delcoco <zdelcoco@gmail.com>
2024-02-19 08:54:43 -0600

    initial commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TodoApi.Models;
+
+public class TodoContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public TodoContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
